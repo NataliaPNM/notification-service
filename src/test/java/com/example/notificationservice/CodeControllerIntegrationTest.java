bb4254772ac08515d6dbc5bfe11d64663cd04b64
Natalia Ponomareva <n.ponomareva@andersenlab.com>
2022-06-02 09:34:53 +0000

    Update CodeControllerIntegrationTest.java

--- src/test/java/com/example/notificationservice/CodeControllerIntegrationTest.java
+++ src/test/java/com/example/notificationservice/CodeControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
@@ -22,8 +23,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = NotificationServiceApplication.class)
-@AutoConfigureMockMvc
+//@SpringBootTest(classes = NotificationServiceApplication.class)
+//@AutoConfigureMockMvc
+//@ImportAutoConfiguration(RefreshAutoConfiguration.class)
+
+@WebMvcTest
 @ImportAutoConfiguration(RefreshAutoConfiguration.class)
 public class CodeControllerIntegrationTest {
     @MockBean
